ab4185cb8fb9d48d1eb12c8e88b2e0a6f6bcb293
haruko.kawano <2023dounyukenshu.026@gmail.com>
2023-05-09 17:07:03 +0900

    NewServlet.javaを作成

--- src/models/Message.java
+++ src/models/Message.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -35,11 +37,11 @@ public class Message {
 
     //作成日時
     @Column(name = "created_at", nullable = false)
-    private String created_at;
+    private Timestamp created_at;
 
     //更新日時
     @Column(name = "updated_at", nullable = false)
-    private String updated_at;
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -65,21 +67,23 @@ public class Message {
         this.content = content;
     }
 
-    public String getCreated_at() {
+    public Timestamp getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(String created_at) {
+    public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
 
-    public String getUpdated_at() {
+    public Timestamp getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(String updated_at) {
+    public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
+
+
 
 
 }
